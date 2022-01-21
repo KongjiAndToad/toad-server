62eed773524cbad5dfb2123c3f584539cdeabbad
MINJU-KIMmm <0112km@ewhain.net>
2022-01-21 10:41:12 +0900

    Remove : "소셜로그인 view 코드 임시 삭제 - 주석처리"

--- toad/user/views.py
+++ toad/user/views.py
@@ -13,6 +13,7 @@ from toad.config.settings import SOCIAL_OUTH_CONFIG
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
+'''
 @api_view(['GET'])
 @permission_classes([AllowAny, ])
 def kakaoGetLogin(request):
@@ -49,3 +50,4 @@ def getUserInfo(reqeust):
     }
     res = requests.get(userUrl, headers=HEADER)
     return Response(res.text)
+'''
