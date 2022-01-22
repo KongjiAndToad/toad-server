ec6573f0bf0d0ce76324e8b5674264514814a8f4
MINJU-KIMmm <0112km@ewhain.net>
2022-01-22 22:58:52 +0900

    Feat : "책 이름 검색 - 전체 책 대상"

--- toad/book/urls.py
+++ toad/book/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from book.views import BookListView
+from book.views import (
+    BookListView,
+    SearchBookView
+)
+
 urlpatterns = [
     path('/user/<int:user_id>', BookListView.as_view()),
+    path('/search', SearchBookView.as_view()),
 ]
--- toad/book/views.py
+++ toad/book/views.py
@@ -20,3 +20,27 @@ class BookListView(View):
         }for book in books]
 
         return JsonResponse({"RESULT" : book_list}, status=200)
+
+class SearchBookView(View):
+    def get(self, request):
+        book_title = request.GET.get('title','')
+
+        if book_title:
+            if len(book_title)>1:
+                books = Book.objects.filter(title__icontains=book_title).order_by("created_at")
+            else:
+                return JsonResponse({"MESSAGE": "검색어는 2글자 이상 입력해주세요"}, status=404)
+        if not books:
+            return JsonResponse({"RESULT": []}, status=200)
+
+        books_list = [{
+            "book_num": book.book_num,
+            "title" : book.title,
+            "user_id" : book.user.id,
+            "liked_count" : book.liked_count,
+        }for book in books]
+
+        return JsonResponse({
+            "RESULT" : books_list,
+            "books_count" : len(books)
+        }, status=200)
