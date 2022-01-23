8e2d2d9570c8091eb383d5aefeaab4abee534b39
MINJU-KIMmm <0112km@ewhain.net>
2022-01-23 18:18:37 +0900

    Feat : "내 서재 내에서 책 제목으로 검색"

--- toad/book/urls.py
+++ toad/book/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from book.views import (
     BookListView,
-    SearchBookView
+    SearchBookView,
+    MyBookSearchView,
 )
 
 urlpatterns = [
     path('/user/<int:user_id>', BookListView.as_view()),
     path('/search', SearchBookView.as_view()),
+    path('/user/<int:user_id>/search', MyBookSearchView.as_view()),
 ]
--- toad/book/views.py
+++ toad/book/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render
 from .models import Book
 from user.models import User
 
+#내 서재 전체 조회
 class BookListView(View):
     def get(self, request, user_id):
         user = User.id
@@ -23,6 +24,35 @@ class BookListView(View):
 
         return JsonResponse({"RESULT" : book_list}, status=200)
 
+#내 서재 내에서 검색
+class MyBookSearchView(View):
+    def get(self, request, user_id):
+        user = User.id
+        book_title = request.GET.get('title','')
+
+        books = Book.objects.filter(user=user_id)
+
+        if book_title:
+            if len(book_title)>1:
+                books = books.filter(title__icontains=book_title).order_by("created_at")
+            else:
+                return JsonResponse({"MESSAGE": "검색어는 2글자 이상 입력해주세요"}, status=404)
+        if not books:
+            return JsonResponse({"RESULT": []}, status=200)
+
+        books_list = [{
+            "book_num": book.book_num,
+            "title": book.title,
+            "user_id": book.user.id,
+            "liked_count": book.liked_count,
+        } for book in books]
+
+        return JsonResponse({
+            "RESULT": books_list,
+            "books_count": len(books)
+        }, status=200)
+
+#전체 책에서 검색
 class SearchBookView(View):
     def get(self, request):
         book_title = request.GET.get('title','')
