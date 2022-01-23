91150bb4516088ec195e942f5c655af6240dc120
MINJU-KIMmm <0112km@ewhain.net>
2022-01-23 18:43:50 +0900

    Add : "sort 기능 추가"

--- toad/book/views.py
+++ toad/book/views.py
@@ -12,8 +12,9 @@ from user.models import User
 class BookListView(View):
     def get(self, request, user_id):
         user = User.id
+        SORT = request.GET.get('sort','')
 
-        books = Book.objects.all().filter(user=user_id).order_by("created_at")
+        books = Book.objects.all().filter(user=user_id).order_by(SORT)
 
         book_list = [{
             "book_num" : book.book_num,
@@ -29,12 +30,13 @@ class MyBookSearchView(View):
     def get(self, request, user_id):
         user = User.id
         book_title = request.GET.get('title','')
+        SORT = request.GET.get('sort','')
 
         books = Book.objects.filter(user=user_id)
 
         if book_title:
             if len(book_title)>1:
-                books = books.filter(title__icontains=book_title).order_by("created_at")
+                books = books.filter(title__icontains=book_title).order_by(SORT)
             else:
                 return JsonResponse({"MESSAGE": "검색어는 2글자 이상 입력해주세요"}, status=404)
         if not books:
@@ -56,10 +58,11 @@ class MyBookSearchView(View):
 class SearchBookView(View):
     def get(self, request):
         book_title = request.GET.get('title','')
+        SORT = request.GET.get('sort','')
 
         if book_title:
             if len(book_title)>1:
-                books = Book.objects.filter(title__icontains=book_title).order_by("created_at")
+                books = Book.objects.filter(title__icontains=book_title).order_by(SORT)
             else:
                 return JsonResponse({"MESSAGE": "검색어는 2글자 이상 입력해주세요"}, status=404)
         if not books:
